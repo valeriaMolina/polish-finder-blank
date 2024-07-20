836528f52a915a487fc29e267f76dd62e5853783
Valeria Molina <valeriamolina123@gmail.com>
2024-07-20 15:19:28 -0400

    blank page

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+<div>
+  <h1 class="logo">&#x1F485; coming soon</h1>
+</div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+</style>
